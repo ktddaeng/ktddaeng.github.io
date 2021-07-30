8abd8294d455473ff69a501fd744eb0f0c121435
Kim Dang <20978371+ktddaeng@users.noreply.github.com>
2021-07-29 18:36:33 -0700

    created link data, formatting roughly

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import '../demo.css';
+import { LINKS } from '../shared/links';
+import clsx from 'clsx';
+import { LoremIpsum, Avatar } from 'react-lorem-ipsum';
 
 function Contact(props) {
+  const linklist = LINKS.map((buttonlink) => {
+    return (
+      <a className="btn btn-outline-light" href={buttonlink.url}>
+        <i className={clsx("me-2", buttonlink.icon)}></i>
+        {buttonlink.name}
+      </a>
+    );
+  });
+
   return (
     <div className="dum dumContact" id="contact">
       <h1>Where to Find Me</h1>
-      <p>Contact Component Here</p>
+      <LoremIpsum avgSentencesPerParagraph={4}/>
+      <div className="d-grid gap-2 d-md-block">
+        {linklist}
+      </div>
     </div>
   );
 }
---
+++ src/shared/links.js
@@ -0,0 +1,21 @@
+export const LINKS =
+[
+  {
+    id: 0,
+    name: 'LinkedIn',
+    icon: 'bi-linkedin',
+    url: 'https://www.linkedin.com/in/kim-dang-aa01a987/'
+  },
+  {
+    id: 1,
+    name: 'GitHub',
+    icon: 'bi-github',
+    url: 'https://github.com/ktddaeng'
+  },
+  {
+    id: 2,
+    name: 'Email',
+    icon: 'bi-envelope',
+    url: 'mailto:thisiskimdang@gmail.com?subject=Mail from Personal Wesite'
+  }
+];
